c0f5b4edb8191ac53b0a1dab339b7d3a72f82671
Dennis Wu <dennis810411@gmail.com>
2021-10-12 17:21:31 +0800

    Finished Structure

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,22 +1,29 @@
 from pytube import YouTube
 
-
 from .step import Step
 from .step import StepException
+
+import time
 
 
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
+        start = time.time()
         for url in data:
-            source = YouTube(url)
-            en_caption = source.captions.get_by_language_code('a.en')
-            en_caption_convert_to_srt = (en_caption.generate_srt_captions())
-            print(en_caption_convert_to_srt)
-            # save the caption to a file named Output.txt
-            text_file = open(utils.get_caption_path(url), "w")
+            if utils.caption_file_exists(url):
+                continue
+
+            try:
+                source = YouTube(url)
+                en_caption = source.captions.get_by_language_code('a.en')
+                en_caption_convert_to_srt = (en_caption.generate_srt_captions())
+            except (KeyError, AttributeError):
+                print('Error when downloading cation for', url)
+                continue
+
+            text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
-            break
 
-
-
+        end = time.time()
+        print('took', end - start, 'seconds')
